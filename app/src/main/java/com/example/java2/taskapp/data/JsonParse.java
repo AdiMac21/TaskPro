f1b9f019055b74461bc037ab87d244f2f9448deb
Xpiral21 <adry_is@yahoo.com>
2016-10-24 20:20:31 +0300

    ++parse

--- app/src/main/java/com/example/java2/taskapp/data/JsonParse.java
+++ app/src/main/java/com/example/java2/taskapp/data/JsonParse.java
@@ -6,12 +6,19 @@ import android.util.Log;
 import com.example.java2.taskapp.model.SubTask;
 import com.example.java2.taskapp.model.Task;
 import com.example.java2.taskapp.model.User;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by java2 on 10/24/2016.
@@ -30,6 +37,7 @@ public class JsonParse {
     public JsonParse(Context context) {
         this.context = context;
         reader=this.read();
+        users=this.getUsers(reader);
 
     }
 
@@ -53,12 +61,19 @@ public class JsonParse {
         return buf.toString();
 
     }
-    private ArrayList<User> parse(String toParse){
-        ArrayList<SubTask> subTasks;
-        ArrayList<Task> tasks;
-        ArrayList<User> users;
+    private ArrayList<User> getUsers(String a) {
+        String TAG = "bla";
+        Gson gson = new Gson();
+        try {
+            JSONArray jsonArray = new JSONArray(a);
+            Log.d(TAG, "jsonArray= " + jsonArray.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        ArrayList<User> contacts = gson.fromJson(a, new TypeToken<ArrayList<User>>() {
 
-
-
+        }.getType());
+        Log.d(TAG, "contacts " + contacts.toString());
+        return contacts;
     }
 }
