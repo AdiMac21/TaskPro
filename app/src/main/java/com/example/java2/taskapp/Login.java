43ad229f18d7b5917ff0ebb5159339475f55ba0b
Xpiral21 <adry_is@yahoo.com>
2016-10-25 23:55:23 +0300

    login layout

--- app/src/main/java/com/example/java2/taskapp/Login.java
+++ app/src/main/java/com/example/java2/taskapp/Login.java
@@ -1,7 +1,10 @@
 package com.example.java2.taskapp;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.java2.taskapp.data.JsonParse;
 import com.example.java2.taskapp.model.User;
@@ -11,9 +14,11 @@ import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
 private ArrayList<User> users;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
         JsonParse json=new JsonParse(this);
         users=json.getUsers();
